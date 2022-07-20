4b38e436700edb25daeee958831e9ece7f7236be
ghada al-masry <ghada.elmasry95@gmail.com>
2022-07-20 04:16:53 +0200

    Update| update URLs for medicines app

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('medicines/', include('medicines.urls')),
+    path('', include('medicines.urls')),
     path('admin/', admin.site.urls),
 ]
